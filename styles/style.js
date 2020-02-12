a3a751b366c815656030be6964a8235ba2e899cb
Hieu Tran <tranhieunz@gmail.com>
2020-02-12 15:46:55 +1300

    Blog Updated style and files

--- styles/style.js
+++ styles/style.js
@@ -1,4 +1,22 @@
-function clickToToggle() {
-   var toggle = document.querySelector(".sprint");
-   toggle.classList.toggle("addStyle");
+
+function clickToToggle1() {
+   var toggle1 = document.querySelector(".sprint1");
+   toggle1.classList.toggle("addStyle");
 };
+function clickToToggle2() {
+   var toggle2 = document.querySelector(".sprint2");
+   toggle2.classList.toggle("addStyle");
+};
+function clickToToggle3() {
+   var toggle3 = document.querySelector(".sprint3");
+   toggle3.classList.toggle("addStyle");
+};
+function clickToToggle4() {
+   var toggle4 = document.querySelector(".sprint4");
+   toggle4.classList.toggle("addStyle");
+};
+function clickToToggle5() {
+   var toggle5 = document.querySelector(".sprint5");
+   toggle5.classList.toggle("addStyle");
+};
+
